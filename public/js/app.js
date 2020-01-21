c0de442ea32f4facd8eda8ec08b76e70c2b79224
Jon Veach <veach09@gmail.com>
2020-01-20 17:03:31 -0800

    Started the bootstrap and forms adventure

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,43 @@
-'use strict'
 
-// localStorage.setItem('t', t);
+    (() => {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    });
+  // })();
+
+
+
+// $('#genBtn').click(function(event) {
+//   // event.preventDefault();
+//   const server = $('#generalServerSelect option:selected').text();
+//   const ticketType = $('#generalTicketSelect option:selected').val();
+  
+//   $(`#${ticketType}`).show();
+
+// });
+
+// $(() => {
+//   var forms = $('#needs-validation');
+//   var validation = Array.prototype.filter.call(forms, function(form) {
+//     form.addEventListener('submit', function(event) {
+//       if (form.checkValidity() === false) {
+//         event.preventDefault();
+//         event.stopPropagation();
+//       }
+//       form.classList.add('was-validated');
+//     }, false);
+//   });
+// });
