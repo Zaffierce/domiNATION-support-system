e8fc335379b919af9cd046c445f38f2af880fe2b
Jon Veach <ozzy_ozbourn@hotmail.com>
2020-07-08 23:54:51 -0700

    Added natural sorting to datatables

--- public/js/datatables.js
+++ public/js/datatables.js
@@ -1,13 +1,16 @@
 $(() => {
     $('#openTickets').DataTable({
-        "order": [[0, "asc"]]
+        "order": [[0, "asc"]],
+        "columnDefs": [{ targets:1, type: 'natural'}]
     });
 
     $('#closedTickets').DataTable({
-        "order": [[5, "desc"]]
+        "order": [[5, "desc"]],
+        "columnDefs": [{ targets:1, type: 'natural'}]
     });
 
     $('#myTickets').DataTable({
-        "order": [[4, "asc"]]
+        "order": [[4, "asc"]],
+        "columnDefs": [{ targets:1, type: 'natural'}]
     });
 });
