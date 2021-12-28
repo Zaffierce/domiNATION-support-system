01ed3fe6c12d9d558c8dfce04fb96bb22a7026f4
Jon Veach <ozzy_ozbourn@hotmail.com>
2021-12-28 15:44:21 -0500

    Removed and separated unneccessary code

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,9 @@ $(() => {
       if (form.checkValidity() === false) {
         event.preventDefault();
         event.stopPropagation();
-      }
+      } 
       form.classList.add("was-validated");
-    }, false);
+    });
   });
 
   $('.notes-edit').on('click', function(event) {
@@ -33,15 +33,10 @@ $(() => {
     let id = $(this).attr('data-note_id');
     let url = $('#notes-delete-form').attr("action");
     $('#notes-delete-form').attr("action", url+id);
-    console.log(url);
   });
 
   $('.notes-save').on('click', function(event) {
     $(this).parent().parent().parent().parent().parent().find('.notes-edit-form').submit();
-  });
-
-  $('#deleteModal').on('hidden.bs.modal', () => {
-    $('#notes-delete-form').attr("action", "/notes-delete/");
   });
 
   $('#ticketTab').on('click', () => {
@@ -79,7 +74,6 @@ $(() => {
     }
   });
 
-
   $(function () {
     $('[data-toggle="popover"]').popover()
   });
@@ -88,177 +82,13 @@ $(() => {
     trigger: 'focus'
   });
 
-  $('.delete').on('click', () => {
-    return confirmDeletion();
-  });
-
   new ClipboardJS('.btn');
 
   $('#copy').on('click', function() {
     $(this).attr('aria-label', 'Command copied!');
     $(this).addClass('tooltipped')
     $(this).addClass('tooltipped-s')
-  })
-
-  // $('#copyText').on('click', function() {
-  //   $('#spawnCode').text().focus();
-  //   $('#spawnCode').text().select();
-  //   document.execCommand('copy');
-  //   console.log('Clicked')
-  // });
-
-  $('.edit-server').on('click', function() {
-    $('#editServerModal-server_id').val($(this).data("serverid"));
-    $('#editServerModal-id').val($(this).data("id"));
-    $('#editServerModal-server_name').val($(this).data("servername"));
-    $('#editServerModal-remove_server_id').val($(this).data("serverid"));
   });
-
-  $('.edit-dinosaur').on('click', function() {
-    $('#editDinosaurModal-dino_name').val($(this).data("dinoname"));
-    $('#editDinosaurModal-dino_id').val($(this).data("dinoid"));
-    $('#editDinosaurModal-dino_spawn').val($(this).data("dinospawn"));
-    $('#editDinosaurModal-dino_lvl').val($(this).data("dinolvl"));
-    $('#editDinosaurModal-remove_dino_id').val($(this).data("dinoid"));
-
-    if ($(this).data("dinoimprint") === false) {
-      $('#editDinosaurModal-dino_imprint option[value=false]').attr("selected", "selected");
-      $('#editDinosaurModal-dino_imprint option[value=true]').removeAttr("selected");
-    } else {
-      $('#editDinosaurModal-dino_imprint option[value=true]').attr("selected", "selected");
-      $('#editDinosaurModal-dino_imprint option[value=false]').removeAttr("selected");
-    }
-  });
-
-  $('.edit-dinosaur-color').on('click', function() {
-    $('#editDinosaurColorModal-actual_id').val($(this).data("colorid"));
-    $('#editDinosaurColorModal-color_id').val($(this).data("dinocolorid"));
-    $('#editDinosaurColorModal-color_name').val($(this).data("dinocolorname"));
-    $('#editDinosaurColorModal-color_hex').val($(this).data("dinocolorhex"));
-    $('#editDinosaurColorModal-remove_id').val($(this).data("colorid"));
-  })
-
-  $('#serverTab').on('click', () => {
-    $('#dinosaurListing').attr("hidden", "hidden");
-    $('#dinosaurColorListing').attr("hidden", "hidden");
-    $('#serverListing').removeAttr("hidden");
-    $('#serverTab').addClass("active");
-    $('#dinoTab').removeClass("active");
-    $('#dinoColorTab').removeClass("active");
-  });
-
-  $('#dinoTab').on('click', () => {
-    $('#dinosaurListing').removeAttr("hidden");
-    $('#dinosaurColorListing').attr("hidden", "hidden");
-    $('#serverListing').attr("hidden", "hidden");
-    $('#dinoTab').addClass("active");
-    $('#serverTab').removeClass("active");
-    $('#dinoColorTab').removeClass("active");
-  });
-
-  $('#dinoColorTab').on('click', () => {
-    $('#dinosaurListing').attr("hidden", "hidden");
-    $('#dinosaurColorListing').removeAttr("hidden");
-    $('#serverListing').attr("hidden", "hidden");
-    $('#dinoColorTab').addClass("active");
-    $('#dinoTab').removeClass("active");
-    $('#serverTab').removeClass("active");
-  });
-
-  // $('.search').on('keyup', delay(function() {
-  //   let queryOpt = $(this)[0].id;
-  //   let searchQuery = $(this).val().toLowerCase();
-  //   let html = '<tr> <th class="ng-scope">Date Submitted</th> <th class="ng-scope">Ticket Type</th> <th class="ng-scope">User</th> <th class="ng-scope">Discord ID</th> <th class="ng-scope">Status</th> <th class="ng-scope">Date Completed</th> <th class="ng-scope">ID</th> </tr>'
-  //   $.ajax({
-  //     url: `https://support.domination-gaming.com/search`,
-  //     method: 'GET',
-  //     data: { data : searchQuery,
-  //             type : queryOpt },
-  //     success: function(data) {
-  //       $('.tickets-table').empty();
-  //       $('.tickets-table').append(html);
-  //       if (data.length === 0) {
-  //         $('.tickets-table').append('<tr class="ng-scope tickets"><td class="ng-scope">No data found</td></tr>');
-  //       } else {
-  //       for (let i = 0; i < data.length; i++) {
-  //         let submitted = `<tr class="ng-scope tickets"> <td class="ng-scope">${data[i].submitted_on}</td>`;
-  //         let type = `<td class="ng-scope">${data[i].type_of_ticket}</td>`;
-  //         let ign = `<td class="ng-scope">${data[i].ign}</td>`;
-  //         let discord = `<td class="ng-scope">${data[i].discord_name}</td>`;
-  //         let status = `<td class="ng-scope">${data[i].status}</td>`;
-  //         let closed = `<td class="ng-scope">${data[i].closed_on}</td>`;
-  //         let id = `<td class="ng-scope"><a href="/details/${data[i].id}">View ticket</a></td></tr>`;
-  //         $('.tickets-table').append(submitted+type+ign+discord+status+closed+id);
-  //       }
-  //     }
-  //   }
-  //   });
-  // }, 500));
-
-  // $('.search_date').on('change', delay(function() {
-  //   let queryOpt = $(this)[0].id;
-  //   let searchQuery = $(this).val().toLowerCase();
-  //   let html = '<tr> <th class="ng-scope">Date Submitted</th> <th class="ng-scope">Ticket Type</th> <th class="ng-scope">User</th> <th class="ng-scope">Discord ID</th> <th class="ng-scope">Status</th> <th class="ng-scope">Date Completed</th> <th class="ng-scope">ID</th> </tr>'
-  //   $.ajax({
-  //     url: `http://localhost:3001/search`,
-  //     method: 'GET',
-  //     data: { data : searchQuery,
-  //             type : queryOpt },
-  //     success: function(data) {
-  //       $('.tickets-table').empty();
-  //       $('.tickets-table').append(html);
-  //       if (data.length === 0) {
-  //         $('.tickets-table').append('<tr class="ng-scope tickets"><td class="ng-scope">No data found</td></tr>');
-  //       } else {
-  //       for (let i = 0; i < data.length; i++) {
-  //         let submitted = `<tr class="ng-scope tickets"> <td class="ng-scope">${data[i].submitted_on}</td>`;
-  //         let type = `<td class="ng-scope">${data[i].type_of_ticket}</td>`;
-  //         let ign = `<td class="ng-scope">${data[i].ign}</td>`;
-  //         let discord = `<td class="ng-scope">${data[i].discord_name}</td>`;
-  //         let status = `<td class="ng-scope">${data[i].status}</td>`;
-  //         let closed = `<td class="ng-scope">${data[i].closed_on}</td>`;
-  //         let id = `<td class="ng-scope"><a href="/details/${data[i].id}">View ticket</a></td></tr>`;
-  //         $('.tickets-table').append(submitted+type+ign+discord+status+closed+id);
-  //       }
-  //     }
-  //   }
-  //   });
-  // }, 500));
 
 });
-
-function confirmSave() {
-  if (confirm("Are you sure you want to save this?\nThis will update across the entire website!")) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function confirmAdd() {
-  if (confirm("Are you sure you want to add this?\nThis will update across the entire website!")) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function confirmDeletion() {
-  if (confirm("Are you sure you want to remove this?\nThis will update across the entire website!")) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// function delay(callback, ms) {
-//   let timer = 0;
-//   return function() {
-//     let context = this, args = arguments;
-//     clearTimeout(timer);
-//     timer = setTimeout(function() {
-//       callback.apply(context, args);
-//     }, ms || 0);
-//   };
-// }
 
